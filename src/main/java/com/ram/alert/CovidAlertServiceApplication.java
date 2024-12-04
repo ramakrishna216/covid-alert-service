4c434e9fa49ca4894d17e000fb35af59905d863c
ramakrishna216 <sadashivadharavath@gmail.com>
2024-12-04 09:35:56 -0500

    Initial commit

---
+++ src/main/java/com/ram/alert/CovidAlertServiceApplication.java
@@ -0,0 +1,13 @@
+package com.ram.alert;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CovidAlertServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CovidAlertServiceApplication.class, args);
+	}
+
+}
